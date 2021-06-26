3b95dd0b4ab90bee14599d35e8442f82beb06a5b
Casseres <andercasseres2015@gmail.com>
2021-06-26 18:50:54 -0300

    Dados principais

---
+++ login.js
@@ -0,0 +1,27 @@
+
+    function acessar(){
+
+        var aux1 = document.getElementById("cmp_email_login");
+        var aux2 = document.getElementById('cmp_senha_login');
+  
+        var email = new String(aux1.value);
+        var senha = new String(aux2.value);
+  
+        if(email != "190543@upf.br"){
+            alert('E-mail invalido! verifique!'); 
+            aux1.value = '';
+          } 
+          
+        else if(senha != '123456'){
+            alert('Senha invalida! verifique!'); 
+            aux2.value = '';
+          } 
+  
+        else if(email == "190543@upf.br" && senha == '123456'){
+            acess = 1;
+            /*window.location.href="https://www.upf.br/";*/
+            window.location.href="index_aprendizados.html";
+            
+          }
+  
+      }
