24e3e0107d5907ec018aed75f2fcd9f1e096d81a
Casseres <andercasseres2015@gmail.com>
2021-06-26 01:50:53 -0300

    Dados Main
    1

---
+++ login.js
@@ -0,0 +1,9 @@
+function acessar(){
+  var email = document.getElementById("nome_login");
+  var senha = document.getElementById("senha_login");
+
+  if(email == 'RobsonSolza@gmail.com'){
+    console.log('log');
+  }
+  
+}
